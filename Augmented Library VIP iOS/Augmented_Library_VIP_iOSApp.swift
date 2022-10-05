73d7863a6b20772fbb6154d9ad4249e097a9f610
Brayton-Lordianto <braylord30@gmail.com>
2022-10-05 18:17:32 -0400

    Initial Commit

---
+++ Augmented Library VIP iOS/Augmented_Library_VIP_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Augmented_Library_VIP_iOSApp.swift
+//  Augmented Library VIP iOS
+//
+//  Created by Brayton Lordianto on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Augmented_Library_VIP_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Augmented Library VIP iOS/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Augmented Library VIP iOS
+//
+//  Created by Brayton Lordianto on 10/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
